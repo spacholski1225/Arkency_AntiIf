1f26103d93250464c6ae823c5045fa9278932c84
Szymon Pacholski <szymon.pacholski@metapack.com>
2023-11-27 18:49:04 +0100

    extracted some conditions to different methods

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -15,59 +15,69 @@ namespace GildedRose
         {
             for (var i = 0; i < _items.Count; i++)
             {
-                if (_items[i].Name != "Aged Brie" && _items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (IsSulfuras(i))
                 {
                     if (_items[i].Quality > 0)
                     {
-                        if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (!IsSulfuras(i))
                         {
-                            _items[i].Quality = _items[i].Quality - 1;
+                            DecreaseQuality(i);
+                        }
+                    }
+                }
+                else if (IsGeneric(i))
+                {
+                    if (_items[i].Quality > 0)
+                    {
+                        if (!IsSulfuras(i))
+                        {
+                            DecreaseQuality(i);
                         }
                     }
                 }
                 else
                 {
-                    if (_items[i].Quality < 50)
+                    if (IsQualityLessThan50(i))
                     {
-                        _items[i].Quality = _items[i].Quality + 1;
+                        IncreaseQuality(i);
 
-                        if (_items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (IsBackstagePass(i))
                         {
                             if (_items[i].SellIn < 11)
                             {
-                                if (_items[i].Quality < 50)
+                                if (IsQualityLessThan50(i))
                                 {
-                                    _items[i].Quality = _items[i].Quality + 1;
+                                    IncreaseQuality(i);
                                 }
                             }
 
                             if (_items[i].SellIn < 6)
                             {
-                                if (_items[i].Quality < 50)
+                                if (IsQualityLessThan50(i))
                                 {
-                                    _items[i].Quality = _items[i].Quality + 1;
+                                    IncreaseQuality(i);
                                 }
                             }
                         }
                     }
                 }
 
-                if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (!IsSulfuras(i))
                 {
-                    _items[i].SellIn = _items[i].SellIn - 1;
+                    DecreaseSellIn(i);
                 }
 
                 if (_items[i].SellIn < 0)
                 {
-                    if (_items[i].Name != "Aged Brie")
+                    if (!IsAgedBrie(i))
                     {
-                        if (_items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (!IsBackstagePass(i))
                         {
                             if (_items[i].Quality > 0)
                             {
-                                if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (!IsSulfuras(i))
                                 {
-                                    _items[i].Quality = _items[i].Quality - 1;
+                                    DecreaseQuality(i);
                                 }
                             }
                         }
@@ -78,13 +88,53 @@ namespace GildedRose
                     }
                     else
                     {
-                        if (_items[i].Quality < 50)
+                        if (IsQualityLessThan50(i))
                         {
-                            _items[i].Quality = _items[i].Quality + 1;
+                            IncreaseQuality(i);
                         }
                     }
                 }
             }
+        }
+
+        private bool IsGeneric(int i)
+        {
+            return !(IsSulfuras(i) || IsBackstagePass(i) || IsAgedBrie(i));
+        }
+
+        private bool IsSulfuras(int i)
+        {
+            return _items[i].Name == "Sulfuras, Hand of Ragnaros";
+        }
+
+        private bool IsBackstagePass(int i)
+        {
+            return _items[i].Name == "Backstage passes to a TAFKAL80ETC concert";
+        }
+
+        private bool IsAgedBrie(int i)
+        {
+            return _items[i].Name == "Aged Brie";
+        }
+
+        private bool IsQualityLessThan50(int i)
+        {
+            return _items[i].Quality < 50;
+        }
+
+        private void DecreaseSellIn(int i)
+        {
+            _items[i].SellIn = _items[i].SellIn - 1;
+        }
+
+        private void IncreaseQuality(int i)
+        {
+            _items[i].Quality = _items[i].Quality + 1;
+        }
+
+        private void DecreaseQuality(int i)
+        {
+            _items[i].Quality = _items[i].Quality - 1;
         }
     }
 }
